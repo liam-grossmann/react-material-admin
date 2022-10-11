e0e1da1db4a98e3a0e52e8914065a3b760f9fa19
Liam Grossmann <liam.grossmann@protonmail.com>
2022-10-11 16:34:28 +0200

    Add react-router-dom

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # react-material-admin
 
+https://v5.reactrouter.com/web/guides/quick-start
+
 https://mui.com/material-ui/getting-started/overview/
 
 https://www.youtube.com/watch?v=r42jcDXSFM0&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO&index=3
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,68 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import { useState } from 'react'
 import { MuiTypography } from './components/MuiTypography';
 import { MuiButton } from './components/MuiButton';
 
-import './App.css'
+//import './App.css'
+import 'normalize.css';
+import { Typography } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <MuiButton></MuiButton>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li> 
+            <li>
+              <Link to="/typography">Typography</Link>
+            </li> 
+            <li>
+              <Link to="/buttons">Buttons</Link>
+            </li> 
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/typography" element={<MuiTypography />}></Route>
+          <Route path="/buttons" element={<MuiButton/>}></Route>
+          <Route path="/" element={<Home/>} ></Route>
+        </Routes> 
+      </div>
+    </Router>
   )
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App
