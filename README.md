a41c924f2372e2e19ff64969512491d8b7c1274e
Liam Grossmann <liam.grossmann@protonmail.com>
2022-10-11 12:42:58 +0200

    Add typography component

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-material-admin
+
+
+
+https://www.youtube.com/watch?v=r42jcDXSFM0&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO&index=3
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant='h1' gutterBottom>h1 Heading</Typography>
+            <Typography variant='h2' gutterBottom>h2 Heading</Typography>
+            <Typography variant='h3' gutterBottom>h3 Heading</Typography>
+            <Typography variant='h4' gutterBottom>h4 Heading</Typography>
+            <Typography variant='h5' gutterBottom>h5 Heading</Typography>
+            <Typography variant='h6' gutterBottom>h6 Heading</Typography>
+
+            <Typography variant='subtitle1' gutterBottom>Sub title 1</Typography>
+            <Typography variant='subtitle2' gutterBottom>Sub title 2</Typography>
+
+            <Typography variant='body1' gutterBottom>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto sit dolore deleniti ut laudantium molestias ad itaque neque eaque voluptatibus debitis, quaerat voluptatem incidunt vero excepturi. Blanditiis, laborum voluptatibus? Perspiciatis!</Typography>
+
+            <Typography variant='body2' gutterBottom>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, repudiandae, qui ab laborum maxime quas non voluptate ad consequuntur totam molestias consectetur cumque explicabo animi aliquam ipsum, dolores earum cum!</Typography>
+        </div>
+    )
+} 
