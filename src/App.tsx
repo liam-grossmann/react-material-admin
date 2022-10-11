9823308737bfbec50f7a35597185798595d70274
Liam Grossmann <liam.grossmann@protonmail.com>
2022-10-11 18:01:53 +0200

    Add side panel and header

--- src/App.tsx
+++ src/App.tsx
@@ -8,61 +8,21 @@ import {
 
 
 import { useState } from 'react'
-import { MuiTypography } from './components/MuiTypography';
-import { MuiButton } from './components/MuiButton';
+//import { Header } from './components/header/header';
+import { SidePanel } from "./components/navigation/sidePanel";
 
 //import './App.css'
 import 'normalize.css';
-import { Typography } from "@mui/material";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li> 
-            <li>
-              <Link to="/typography">Typography</Link>
-            </li> 
-            <li>
-              <Link to="/buttons">Buttons</Link>
-            </li> 
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/users" element={<Users />}></Route>
-          <Route path="/typography" element={<MuiTypography />}></Route>
-          <Route path="/buttons" element={<MuiButton/>}></Route>
-          <Route path="/" element={<Home/>} ></Route>
-        </Routes> 
-      </div>
+      <SidePanel></SidePanel>
     </Router>
   )
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App
