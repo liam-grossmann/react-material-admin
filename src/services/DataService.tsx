1bbf993d7d35e936324d8ccbda3dd9cbd13547a1
Liam Grossmann <liam.grossmann@protonmail.com>
2022-10-17 15:39:45 +0200

    Avatars

--- src/services/DataService.tsx
+++ src/services/DataService.tsx
@@ -5,8 +5,8 @@ import johnSmith from './../assets/avatars/johnsmith.jpeg';
 import karenmonroe from './../assets/avatars/karenmonroe.jpeg';
 import juliawest from './../assets/avatars/juliawest.jpeg';
 
-//import avatar1 from './../assets/avatars/1.jpeg';
-//import avatar2 from './../assets/avatars/2.jpeg';
+import avatar1 from './../assets/avatars/1.jpeg';
+import avatar2 from './../assets/avatars/2.jpeg';
 import avatar3 from './../assets/avatars/3.jpeg';
 import avatar4 from './../assets/avatars/4.jpeg';
 import avatar5 from './../assets/avatars/5.jpeg';
@@ -65,66 +65,49 @@ import avatar57 from './../assets/avatars/57.jpeg';
 import avatar58 from './../assets/avatars/58.jpeg';
 import avatar59 from './../assets/avatars/59.jpeg';
 import avatar60 from './../assets/avatars/60.jpeg';
-import avatar61 from './../assets/avatars/61.jpeg';
+//import avatar61 from './../assets/avatars/61.jpeg';
 import avatar62 from './../assets/avatars/62.jpeg';
-import avatar63 from './../assets/avatars/63.jpeg';
+//import avatar63 from './../assets/avatars/63.jpeg';
 import avatar64 from './../assets/avatars/64.jpeg';
 import avatar65 from './../assets/avatars/65.jpeg';
 import avatar66 from './../assets/avatars/66.jpeg';
-import avatar67 from './../assets/avatars/67.jpeg';
+//import avatar67 from './../assets/avatars/67.jpeg';
 import avatar68 from './../assets/avatars/68.jpeg';
 import avatar69 from './../assets/avatars/69.jpeg';
-import avatar70 from './../assets/avatars/70.jpeg';
+//import avatar70 from './../assets/avatars/70.jpeg';
 import avatar71 from './../assets/avatars/71.jpeg';
 import avatar72 from './../assets/avatars/72.jpeg';
-import avatar73 from './../assets/avatars/73.jpeg';
+//import avatar73 from './../assets/avatars/73.jpeg';
 import avatar74 from './../assets/avatars/74.jpeg';
 import avatar75 from './../assets/avatars/75.jpeg';
-import avatar76 from './../assets/avatars/76.jpeg';
-import avatar77 from './../assets/avatars/77.jpeg';
-import avatar78 from './../assets/avatars/78.jpeg';
-import avatar79 from './../assets/avatars/79.jpeg';
-import avatar80 from './../assets/avatars/80.jpeg';
+//import avatar76 from './../assets/avatars/76.jpeg';
+//import avatar77 from './../assets/avatars/77.jpeg';
+//import avatar78 from './../assets/avatars/78.jpeg';
+//import avatar79 from './../assets/avatars/79.jpeg';
+//import avatar80 from './../assets/avatars/80.jpeg';
 import avatar81 from './../assets/avatars/81.jpeg';
 import avatar82 from './../assets/avatars/82.jpeg';
-import avatar83 from './../assets/avatars/83.jpeg';
+//import avatar83 from './../assets/avatars/83.jpeg';
 import avatar84 from './../assets/avatars/84.jpeg';
-import avatar85 from './../assets/avatars/85.jpeg';
-import avatar86 from './../assets/avatars/86.jpeg';
+//import avatar85 from './../assets/avatars/85.jpeg';
+//import avatar86 from './../assets/avatars/86.jpeg';
 import avatar87 from './../assets/avatars/87.jpeg';
 import avatar88 from './../assets/avatars/88.jpeg';
 import avatar89 from './../assets/avatars/89.jpeg';
-import avatar90 from './../assets/avatars/90.jpeg';
+//import avatar90 from './../assets/avatars/90.jpeg';
 import avatar91 from './../assets/avatars/91.jpeg';
 import avatar92 from './../assets/avatars/92.jpeg';
-import avatar93 from './../assets/avatars/93.jpeg';
-import avatar94 from './../assets/avatars/94.jpeg';
+//import avatar93 from './../assets/avatars/93.jpeg';
+//import avatar94 from './../assets/avatars/94.jpeg';
 import avatar95 from './../assets/avatars/95.jpeg';
 import avatar96 from './../assets/avatars/96.jpeg';
-import avatar97 from './../assets/avatars/97.jpeg';
-import avatar98 from './../assets/avatars/98.jpeg';
+//import avatar97 from './../assets/avatars/97.jpeg';
+//import avatar98 from './../assets/avatars/98.jpeg';
 import avatar99 from './../assets/avatars/99.jpeg';
 import avatar100 from './../assets/avatars/100.jpeg';
 
 import users from './users.json';
 
-const arr = new Array();
-//arr.push(avatar1);
-//arr.push(avatar2);
-arr.push(avatar3);
-arr.push(avatar4);
-arr.push(avatar5);
-arr.push(avatar6);
-arr.push(avatar7);
-//arr.push(avatar8);
-//arr.push(avatar9);
-arr.push(avatar10);
-
-arr.push(avatar12);
-arr.push(avatar13);
-arr.push(avatar14);
-arr.push(avatar15);
-arr.push(avatar16);
 
 
 
@@ -136,13 +119,14 @@ export class DataService {
     
     getUsers(): IUser[] {
         let listToReturn = new Array<IUser>();
+        let avatars = this.avatarArray;
 
 
         users.forEach((user, i) => {
             i = i + 1;
-            if (i < 5) {
+            if (i < 100) {
                 let userToAdd = new User(user);
-                userToAdd.avatar =  arr[Number(user.avatar)];
+                userToAdd.avatar =  avatars[Number(user.avatar)];
                 listToReturn.push(userToAdd);
             }
         });
@@ -202,5 +186,128 @@ export class DataService {
           );
         return valueToReturn;
     }*/
+
+    get avatarArray() : Array<any> {
+        const arr = new Array();
+        arr.push(avatar1);
+        arr.push(avatar2);
+        arr.push(avatar3);
+        arr.push(avatar4);
+        arr.push(avatar5);
+        arr.push(avatar6);
+        arr.push(avatar7);
+        arr.push(avatar8);
+        arr.push(avatar9);
+        arr.push(avatar10);
+        arr.push(avatar11);
+        arr.push(avatar12);
+        arr.push(avatar13);
+        arr.push(avatar14);
+        arr.push(avatar15);
+        arr.push(avatar16);
+        arr.push(avatar17);
+        arr.push(avatar18);
+        arr.push(avatar19);
+        arr.push(avatar20);
+        arr.push(avatar21);
+        arr.push(avatar22);
+        arr.push(avatar23);
+        arr.push(avatar24);
+        arr.push(avatar25);
+        arr.push(avatar26);
+        arr.push(avatar27);
+        arr.push(avatar28);
+        arr.push(avatar29);
+
+        arr.push(avatar20);
+        arr.push(avatar21);
+        arr.push(avatar22);
+        arr.push(avatar23);
+        arr.push(avatar24);
+        arr.push(avatar25);
+        arr.push(avatar26);
+        arr.push(avatar27);
+        arr.push(avatar28);
+        arr.push(avatar29);
+
+        arr.push(avatar30);
+        arr.push(avatar31);
+        arr.push(avatar32);
+        arr.push(avatar33);
+        arr.push(avatar34);
+        arr.push(avatar35);
+        arr.push(avatar36);
+        arr.push(avatar37);
+        arr.push(avatar38);
+        arr.push(avatar39);
+
+        arr.push(avatar40);
+        arr.push(avatar41);
+        arr.push(avatar42);
+        arr.push(avatar43);
+        arr.push(avatar44);
+        arr.push(avatar45);
+        arr.push(avatar46);
+        arr.push(avatar47);
+        arr.push(avatar48);
+        arr.push(avatar49);
+
+        arr.push(avatar50);
+        arr.push(avatar51);
+        arr.push(avatar52);
+        arr.push(avatar53);
+        arr.push(avatar54);
+        arr.push(avatar55);
+        arr.push(avatar56);
+        arr.push(avatar57);
+        arr.push(avatar58);
+        arr.push(avatar59);
+
+        arr.push(avatar60);
+     //   arr.push(avatar61);
+        arr.push(avatar62);
+     //   arr.push(avatar63);
+        arr.push(avatar64);
+        arr.push(avatar65);
+        arr.push(avatar66);
+      //  arr.push(avatar67);
+        arr.push(avatar68);
+        arr.push(avatar69);
+
+      //  arr.push(avatar70);
+        arr.push(avatar71);
+        arr.push(avatar72);
+      //  arr.push(avatar73);
+        arr.push(avatar74);
+        arr.push(avatar75);
+      //  arr.push(avatar76);
+      //  arr.push(avatar77);
+       // arr.push(avatar78);
+       // arr.push(avatar79);
+
+       // arr.push(avatar80);
+        arr.push(avatar81);
+        arr.push(avatar82);
+      //  arr.push(avatar83);
+        arr.push(avatar84);
+       // arr.push(avatar85);
+      //  arr.push(avatar86);
+        arr.push(avatar87);
+        arr.push(avatar88);
+        arr.push(avatar89);
+
+     //   arr.push(avatar90);
+        arr.push(avatar91);
+        arr.push(avatar92);
+       // arr.push(avatar93);
+       // arr.push(avatar94);
+        arr.push(avatar95);
+        arr.push(avatar96);
+      //  arr.push(avatar97);
+      //  arr.push(avatar98);
+        arr.push(avatar99);
+
+        return arr;
+    }
 
 }
